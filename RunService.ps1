62d338ed83539e26b302ad2e454a99be7fb79b79
matticamp <2312425+matticamp@users.noreply.github.com>
2018-05-23 19:45:54 -0400

    Add files via upload

---
+++ RunService.ps1
@@ -0,0 +1,18 @@
+﻿$ServiceName= Read-host "Enter service name"
+$svc= Get-Service -Name $ServiceName | Sort-Object Status, DisplayName
+
+
+If($status -eq "Stopped")
+
+{
+     Start-Service -Name $ServiceName
+     Write-Host $status
+
+
+}Else  {
+
+Write-Host "The specified service is already running"
+
+}
+
+$svc
